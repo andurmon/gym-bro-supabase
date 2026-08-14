1f9b9e02b4913256ade80a215fe9d65948661c89
andurmon <andurmon@gmail.com>
2026-08-13 19:57:33 -0500

    testing get workouts function

--- supabase/functions/workouts/index.ts
+++ supabase/functions/workouts/index.ts
@@ -1,5 +1,5 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
-import { getWorkouts } from './getWorkouts';
+import { getWorkouts } from './getWorkouts.ts';
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
