c0d7370dd109dee167907ace1ebcedaaeb864faf
andurmon <andurmon@gmail.com>
2026-08-27 17:04:29 -0500

    Wokrout exercises Join

--- supabase/functions/workouts/createWorkout.ts
+++ supabase/functions/workouts/createWorkout.ts
@@ -18,6 +18,7 @@ export async function createWorkout(
 ): Promise<Response> {
   let body: Workout;
 
+  //1. Validates Request
   try {
     body = await req.json();
   } catch {
@@ -42,22 +43,54 @@ export async function createWorkout(
     return response({ error: 'Invalid type of training' }, 400);
   }
 
+  const exercises = body?.exercises ?? [];
+  if (!!exercises && !Array.isArray(exercises)) {
+    return response({ error: 'Exercises must be an array' }, 400);
+  }
+  const isValidStructure = exercises.every(
+    item => item !== null && typeof item === 'object' && !!item?.id
+  );
+
+  if (!isValidStructure)
+    return response({ error: "Exercises must have an 'id'" }, 400);
+
+  //2. Creates Workout
   const workout = {
     name: body.name.trim(),
+    key: body.key.trim(),
     description: body.description ?? null,
     category: body.category ?? null,
     type_of_training: body.typeOfTraining ?? null,
   };
 
+  // 3. Creates Workout-Exercise relationship
   const { data, error } = await supabase
     .from(TABLE_NAME)
     .insert(workout)
-    .select(`id, created_at, name, description, category, type_of_training`)
+    .select(
+      `id, created_at, name, key, description, category, type_of_training`
+    )
     .single();
+  console.log('data: ', data);
 
-  if (error) {
-    console.error(error);
+  if (!data?.id || error) {
+    console.error(error ? error : { data });
     return response({ error: 'Failed to create workout' }, 500);
+  }
+
+  if (exercises?.length > 0) {
+    const relation = exercises.map((exercise, index) => ({
+      exercise_id: exercise.id,
+      workout_id: data.id,
+      workout_sequence_id: index,
+      sets: exercise.sets,
+      reps: exercise.reps,
+    }));
+
+    const joinTableResult = await supabase
+      .from('workout_exercise')
+      .insert(relation);
+    console.log('joinTableResult: ', joinTableResult);
   }
 
   return response(data, 201);
--- supabase/functions/workouts/getWorkouts.ts
+++ supabase/functions/workouts/getWorkouts.ts
@@ -16,26 +16,53 @@ export async function getWorkouts(
   const search = url.searchParams.get('search');
   const category = url.searchParams.get('category');
   const typeOfTraining = url.searchParams.get('typeOfTraining');
+  const expand = url.searchParams.get('expand');
 
-  let query = supabase
-    .from(TABLE_NAME)
-    .select(`id, created_at, name, description, category, type_of_training`)
-    .order('name', { ascending: true });
+  let workoutsQuery = expand
+    ? supabase
+        .from(TABLE_NAME)
+        .select(
+          ` id,
+            name,
+            description,
+            category,
+            type_of_training,
+            key,
+            created_at,
+            workout_exercise (
+                id,
+                sets,
+                reps,
+                workout_sequence_id,
+                exercises (
+                    id,
+                    name,
+                    description,
+                    image_url,
+                    muscle_group_id,
+                    equipment_id
+                )
+            )`
+        )
+        .order('name', { ascending: true })
+    : supabase
+        .from(TABLE_NAME)
+        .select(`id, created_at, name, description, category, type_of_training`)
+        .order('name', { ascending: true });
 
   if (search) {
-    query = query.ilike('name', `%${search}%`);
+    workoutsQuery = workoutsQuery.ilike('name', `%${search}%`);
   }
 
   if (category) {
-    query = query.eq('category', category);
+    workoutsQuery = workoutsQuery.eq('category', category);
   }
 
   if (typeOfTraining) {
-    query = query.eq('type_of_training', typeOfTraining);
+    workoutsQuery = workoutsQuery.eq('type_of_training', typeOfTraining);
   }
 
-  const { data, error } = await query;
-
+  const { data, error } = await workoutsQuery;
   if (error) {
     console.error(error);
     return response({ error: 'Failed to retrieve workouts' }, 500);
